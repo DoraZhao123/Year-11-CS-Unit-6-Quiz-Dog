a30d43e80dba971b2226d1795af24bc770271b70
dorazhao <doraz2025@student.cis.edu.hk>
2023-02-15 15:06:05 +0800

    Completed

--- src/DogMaker.java
+++ src/DogMaker.java
@@ -1,6 +1,16 @@
 public class DogMaker {
   
   public static void main(String[] args) {
+    Dog dog = new Dog(2, "Jill", "black and tan");
+    dog.setName("Jessie");
+    dog.getAge();
+    System.out.println(dog.getAge());
+    dog.birthday();
+    System.out.println(dog.getAge());
+    System.out.println(dog.toString());
+
+
+
     
     
   }
